957c05c6c75bf326322896944505c9629aa2dd62
Jon Whitter <jon.whitter@cantarus.com>
2024-04-16 11:41:47 +0100

    feat: added repository

---
+++ MyPackage.Client/assets/src/repository/sources/time.datasource.ts
@@ -0,0 +1,25 @@
+import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
+import { UmbDataSourceResponse } from "@umbraco-cms/backoffice/repository";
+import { tryExecuteAndNotify } from "@umbraco-cms/backoffice/resources";
+import { TimeService } from "../../api";
+
+export interface TimeDataSource {
+  getTime(): Promise<UmbDataSourceResponse<string>>;
+  getDate(): Promise<UmbDataSourceResponse<string>>;
+}
+
+export class TimeManagementDataSource implements TimeDataSource {
+  #host: UmbControllerHost;
+
+  constructor(host: UmbControllerHost) {
+    this.#host = host;
+  }
+
+  async getTime(): Promise<UmbDataSourceResponse<string>> {
+    return await tryExecuteAndNotify(this.#host, TimeService.getTime());
+  }
+
+  async getDate(): Promise<UmbDataSourceResponse<string>> {
+    return await tryExecuteAndNotify(this.#host, TimeService.getDate());
+  }
+}
---
+++ MyPackage.Client/assets/src/repository/time.repository.ts
@@ -0,0 +1,23 @@
+import { UmbControllerBase } from "@umbraco-cms/backoffice/class-api";
+import { UmbControllerHost } from "@umbraco-cms/backoffice/controller-api";
+import { TimeManagementDataSource } from "./sources/time.datasource";
+
+export class TimeManagementRespository extends UmbControllerBase {
+  #timeDataSource: TimeManagementDataSource;
+
+  constructor(host: UmbControllerHost) {
+    super(host);
+    
+    this.#timeDataSource = new TimeManagementDataSource(this);
+
+    console.log("repository constructor");
+  }
+
+  async getTime() {
+    return this.#timeDataSource.getTime();
+  }
+
+  async getDate() {
+    return this.#timeDataSource.getDate();
+  }
+}
